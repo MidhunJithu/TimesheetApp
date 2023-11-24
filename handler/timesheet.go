51d9071a71ec07c7fcf7fa9d309f456f812b3ce1
Midhunjithu <midhun.m@techversantinfo.com>
2023-11-24 14:24:26 +0530

    added changes to retrieve data, added redis client

--- handler/timesheet.go
+++ handler/timesheet.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
@@ -24,8 +25,8 @@ func NewSheetHandler() *SheetSrv {
 	return &SheetSrv{}
 }
 
-func (*SheetSrv) SubmitSheetId(ctx *gin.Context) {
-	srv, err := GetSheetClient(ctx)
+func (s *SheetSrv) SubmitSheetId(ctx *gin.Context) {
+	srv, err := s.GetSheetClient(ctx)
 	if err != nil {
 		return
 	}
@@ -52,19 +53,59 @@ func (*SheetSrv) SubmitSheetId(ctx *gin.Context) {
 		utils.AbortWithError(ctx, err)
 		return
 	}
+	timeSheet := models.Timesheet{}
 	if len(resp.Values) == 0 {
 		fmt.Println("No data found.")
 	} else {
+		LatestEntry := models.SheetLastInfo{}
 		for _, row := range resp.Values {
-			fmt.Printf("%s\n", row)
+			if len(row) > 0 {
+				day, err := time.Parse("2/1/2006", row[0].(string))
+				if err != nil {
+					continue
+				}
+				if day.After(time.Now()) {
+					break
+				}
+
+				if weekday := day.Weekday(); weekday == time.Saturday || weekday == time.Sunday {
+					continue
+				} else if len(row) < 5 {
+					break
+				} else if row[1] != nil && len(row[1].(string)) > 0 {
+					LatestEntry.Date = fmt.Sprintf("%s", row[0])
+					LatestEntry.Task = fmt.Sprintf("%s", row[2])
+					LatestEntry.Hours = fmt.Sprintf("%s", row[3])
+					LatestEntry.Leave = fmt.Sprintf("%s", row[5])
+				} else {
+					break
+				}
+			}
+			timeSheet.Lastupdate = &LatestEntry
 		}
 	}
-	ctx.JSON(http.StatusOK, gin.H{
-		"data": "ok",
-	})
+	timeSheet.NewEntry = &models.SheetNewinfo{
+		Hours: "8",
+		Leave: "No",
+	}
+	lastDate, err := time.Parse("2/1/2006", timeSheet.Lastupdate.Date)
+	if err != nil {
+		log.Errorf("some error while pasring the last date %s", err)
+	}
+	weekday := lastDate.Weekday()
+	switch weekday {
+	case time.Friday:
+		timeSheet.NewEntry.Date = lastDate.AddDate(0, 0, 3).String()
+	case time.Saturday:
+		timeSheet.NewEntry.Date = lastDate.AddDate(0, 0, 2).String()
+	default:
+		timeSheet.NewEntry.Date = lastDate.AddDate(0, 0, 1).String()
+	}
+	ctx.JSON(http.StatusOK, timeSheet)
 }
 
-func GetSheetClient(ctx *gin.Context) (*sheets.Service, error) {
+func (s *SheetSrv) GetSheetClient(ctx *gin.Context) (*sheets.Service, error) {
+	srv := &sheets.Service{}
 	file, err := os.ReadFile("credentials.json")
 	if err != nil {
 		utils.AbortWithError(ctx, err)
@@ -77,10 +118,9 @@ func GetSheetClient(ctx *gin.Context) (*sheets.Service, error) {
 	}
 	client := utils.GetClient(cred)
 
-	srv, err := sheets.NewService(ctx, option.WithHTTPClient(client))
+	srv, err = sheets.NewService(ctx, option.WithHTTPClient(client))
 	if err != nil {
 		log.Fatalf("Unable to retrieve Sheets client: %v", err)
 	}
-
 	return srv, nil
 }
--- models/handler.go
+++ models/handler.go
@@ -1,1 +1,20 @@
 package models
+
+type SheetLastInfo struct {
+	Date  string `json:"last_updated_date,omitempty"`
+	Task  string `json:"last_task_info,omitempty"`
+	Hours string `json:"last_working_hours,omitempty"`
+	Leave string `json:"leave_on_last_date,omitempty"`
+}
+
+type SheetNewinfo struct {
+	Date  string `json:"date_to_update,omitempty"`
+	Task  string `json:"ltask_done,omitempty"`
+	Hours string `json:"total_hours,omitempty"`
+	Leave string `json:"on_leave,omitempty"`
+}
+
+type Timesheet struct {
+	Lastupdate *SheetLastInfo `json:"last_update,omitempty"`
+	NewEntry   *SheetNewinfo  `json:"upcoming_change,omitempty"`
+}
