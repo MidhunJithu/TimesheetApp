87cd82b450d9ba07430a1f6f5e093845efedabd4
Midhunjithu <midhun.m@techversantinfo.com>
2023-12-05 19:32:21 +0530

    generalisation of apis

--- handler/timesheet.go
+++ handler/timesheet.go
@@ -75,33 +75,39 @@ func (s *SheetSrv) SubmitSheetId(ctx *gin.Context) {
 		if len(resp.Values) == 0 {
 			fmt.Println("No data found.")
 		} else {
+			Sheetdata := s.GetFromDB("timeSheetInfo", "sheetId", sheetId.(string),
+				[]string{"DateCol", "TaskCol", "HoursCol", "LeaveCol", "TotalField"})
 			LatestEntry := models.SheetLastInfo{}
 			for i, row := range resp.Values {
 				if len(row) > 0 {
-					day, err := time.Parse("2/1/2006", row[0].(string))
+					dateRowIndex := Sheetdata["DateCol"].(int64)
+					TaskRowIndex := Sheetdata["TaskCol"].(int64)
+					HrRowIndex := Sheetdata["HoursCol"].(int64)
+					LeaveRowIndex := Sheetdata["LeaveCol"].(int64)
+					TotalField := Sheetdata["TotalField"].(int64)
+					day, err := time.Parse("2/1/2006", row[dateRowIndex].(string))
 					if err != nil {
 						continue
 					}
 					if day.After(time.Now()) {
 						break
 					}
-
 					if weekday := day.Weekday(); weekday == time.Saturday || weekday == time.Sunday {
 						continue
-					} else if len(row) < 5 {
+					} else if len(row) < int(TotalField) {
 						break
-					} else if row[1] != nil && len(row[1].(string)) > 0 {
-						LatestEntry.Date = fmt.Sprintf("%s", row[0])
-						LatestEntry.Task = fmt.Sprintf("%s", row[2])
-						LatestEntry.Hours = fmt.Sprintf("%s", row[3])
-						LatestEntry.Leave = fmt.Sprintf("%s", row[5])
+					} else if row[TaskRowIndex] != nil && len(row[TaskRowIndex].(string)) > 0 {
+						LatestEntry.Date = fmt.Sprintf("%s", row[dateRowIndex])
+						LatestEntry.Task = fmt.Sprintf("%s", row[TaskRowIndex])
+						LatestEntry.Hours = fmt.Sprintf("%s", row[HrRowIndex])
+						LatestEntry.Leave = fmt.Sprintf("%s", row[LeaveRowIndex])
 						LatestEntry.A1Range = fmt.Sprintf("%s!%v:%v", sheetName, i+1, i+1)
 					} else {
 						break
 					}
 				}
-				timeSheet.Lastupdate = &LatestEntry
 			}
+			timeSheet.Lastupdate = &LatestEntry
 			cacheString, err := json.Marshal(LatestEntry)
 			if err != nil {
 				log.Errorf("error on cache data marshalling := %s", err)
@@ -153,12 +159,12 @@ func (s *SheetSrv) AddSheetEntry(ctx *gin.Context) {
 		utils.AbortWithError(ctx, err)
 		return
 	}
-	dateEntry := fmt.Sprintf("%v-%v-%v", date.Day(), date.Month(), date.Year())
+	dateEntry := fmt.Sprintf("%v/%d/%v", date.Day(), int(date.Month()), date.Year())
 	_, err = srv.Spreadsheets.Values.Update(sheetEntry.SheetId, sheetEntry.A1Range, &sheets.ValueRange{
 		Values: [][]interface{}{
 			{dateEntry, date.Weekday().String(), sheetEntry.Task, sheetEntry.Hours, sheetEntry.Leave, sheetEntry.Leave},
 		},
-	}).ValueInputOption("USER_ENTERED").Do()
+	}).ValueInputOption("RAW").Do()
 	if err != nil {
 		utils.AbortWithError(ctx, err)
 		return
@@ -185,7 +191,19 @@ func (s *SheetSrv) AddSheetEntry(ctx *gin.Context) {
 		}
 		prevRow := currRow - 6
 		range_ := strings.Replace(sheetEntry.A1Range, fmt.Sprintf("%v:", currRow), fmt.Sprintf("%v:", prevRow), 1)
+		if date.Day() < 7 {
+			lastWkData := getLastweekdata(srv, sheetEntry.SheetId)
+			rangeParam := fmt.Sprintf("%s!A%d:Z%d", sheetEntry.SheetName, models.TopRow, models.TopRow+7)
+			currweekdata := getSheetData(srv, sheetEntry.SheetId, rangeParam)
+			updateSheetdata(srv, sheetEntry.SheetId, rangeParam, lastWkData)
+			AppendSheetData(srv, sheetEntry.SheetId, rangeParam, currweekdata)
+			TotalRow := models.TopRow + len(lastWkData) + len(currweekdata)
+			range_ = fmt.Sprintf("%s!A%d:Z%d", sheetEntry.SheetName, TotalRow-7, TotalRow)
+		}
+
 		data := getSheetData(srv, sheetEntry.SheetId, range_)
+		// hour column hardcoded here, needs to take it from db
+		totalHr := getTotalHours(data, 3)
 		table := convertToHTMLTable(data)
 		MailData := s.GetFromDB("email_templates", "sheetId", sheetEntry.SheetId,
 			[]string{"template", "to", "cc", "name", "project", "client"})
@@ -196,7 +214,7 @@ func (s *SheetSrv) AddSheetEntry(ctx *gin.Context) {
 		startDate := date.AddDate(0, 0, -7)
 		billing_period := fmt.Sprintf("%s to %s", startDate.Format("2006-1-2"), date.Format("2006-01-02"))
 		templateBody = strings.ReplaceAll(templateBody, "%billing period%", billing_period)
-		templateBody = strings.ReplaceAll(templateBody, "%total_hours%", "40")
+		templateBody = strings.ReplaceAll(templateBody, "%total_hours%", fmt.Sprintf("%v", totalHr))
 		toMails := MailData["to"].([]interface{})
 		ccMails := MailData["cc"].(map[string]interface{})
 		to := make([]string, 0)
@@ -207,16 +225,77 @@ func (s *SheetSrv) AddSheetEntry(ctx *gin.Context) {
 		for k, _ := range ccMails {
 			cc[k] = ccMails[k].(string)
 		}
-
 		Sendmail("midhun.m@techversantinfo.com", to, cc, fmt.Sprintf("weekly timesheet for the period %s", billing_period), templateBody)
+		err = AddSatnSun(srv, sheetEntry)
+		if err != nil {
+			panic(err)
+		}
 	}
 	ctx.JSON(200, timeSheet)
+}
+
+func getLastweekdata(srv *sheets.Service, sheetId string) [][]interface{} {
+	spreadsheet, err := srv.Spreadsheets.Get(sheetId).Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve spreadsheet: %v", err)
+	}
+	prevSheet := spreadsheet.Sheets[1].Properties
+	sheetName := prevSheet.Title
+	val, err := srv.Spreadsheets.Values.Get(sheetId, sheetName+"!A1:Z90").Do()
+	if err != nil {
+		panic(err)
+	}
+	lastRow := len(val.Values)
+	if lastRow > 7 {
+		lastRow -= 7
+	} else {
+		lastRow = 1
+	}
+	rangeParam := fmt.Sprintf("%s!A%d:Z%d", sheetName, lastRow, lastRow+6)
+	return getSheetData(srv, sheetId, rangeParam)
+}
+
+func AddSatnSun(srv *sheets.Service, sheetEntry *models.SheetNewinfo) (err error) {
+	date, err := time.Parse("2006-1-2", sheetEntry.Date)
+	if err != nil {
+		log.Errorf("error on unmarshallig post body :-%s", err)
+		return
+	}
+	date = date.AddDate(0, 0, 1)
+	dateEntry := fmt.Sprintf("%v/%d/%v", date.Day(), int(date.Month()), date.Year())
+	Nxtday := date.AddDate(0, 0, 1)
+	NxtdateEntry := fmt.Sprintf("%v/%d/%v", Nxtday.Day(), int(Nxtday.Month()), Nxtday.Year())
+
+	srv.Spreadsheets.Values.Append(sheetEntry.SheetId, sheetEntry.A1Range, &sheets.ValueRange{
+		Values: [][]interface{}{
+			{dateEntry, date.Weekday().String(), "Weekend Off", 0, "No", "No"},
+			{NxtdateEntry, Nxtday.Weekday().String(), "Weekend Off", 0, "No", "No"},
+		},
+	}).ValueInputOption("RAW").Do()
+	return
+}
+
+func AppendSheetData(srv *sheets.Service, sheetId, rangeParam string, data [][]interface{}) {
+	_, err := srv.Spreadsheets.Values.Append(sheetId, rangeParam, &sheets.ValueRange{
+		Values: data,
+	}).ValueInputOption("RAW").Do()
+	if err != nil {
+		panic(err)
+	}
 }
 
 func GetNextUpdateinfo(currentSheet models.SheetLastInfo, sheetName string) *models.SheetNewinfo {
 	NewEntry := &models.SheetNewinfo{
 		Hours: "8",
 		Leave: "No",
+	}
+	if currentSheet.Date == "" {
+		// if no current time, passes the first day of current month
+		now := time.Now()
+		NewDate := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, time.UTC)
+		NewEntry.Date = NewDate.String()
+		NewEntry.A1Range = fmt.Sprintf("%s!%v:%v", sheetName, models.TopRow, models.TopRow)
+		return NewEntry
 	}
 	lastDate, err := time.Parse("2/1/2006", currentSheet.Date)
 	if err != nil {
@@ -361,4 +440,22 @@ func (s *SheetSrv) GetFromDB(collection string, key string, keyVal string, reqFi
 		}
 	}
 	return nil
+}
+
+func getTotalHours(data [][]interface{}, hourcol int) int {
+	hr := 0
+	for _, row := range data {
+		for i, col := range row {
+			if i == hourcol {
+				nwhr, err := strconv.Atoi(col.(string))
+				if err != nil {
+					panic(err)
+				}
+				hr += nwhr
+			} else {
+				continue
+			}
+		}
+	}
+	return hr
 }
